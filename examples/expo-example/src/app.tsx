7da4b3fb3dfb590755d3d8a8881124aa91e2e7cb
Zoe Roux <zoe.roux@sdg.moe>
2022-11-04 03:32:50 +0900

    Add expo example

---
+++ examples/expo-example/src/app.tsx
@@ -0,0 +1,28 @@
+//
+// Copyright (c) Zoe Roux and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
+
+function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
+
+export default registerRootComponent(App);
