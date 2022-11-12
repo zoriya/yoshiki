3c6e58b6b79c6f2659616304ff41590a42b42d2d
Zoe Roux <zoe.roux@sdg.moe>
2022-11-12 21:20:46 +0900

    Update packages

---
+++ packages/yoshiki/web.ts
@@ -0,0 +1,6 @@
+//
+// Copyright (c) Zoe Roux and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+export * from "./src";
