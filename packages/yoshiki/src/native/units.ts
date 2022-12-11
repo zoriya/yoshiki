5e3505b444244e3459dd1f1fb1c732b34e3889f3
Zoe Roux <zoe.roux@sdg.moe>
2022-12-11 23:55:06 +0900

    Add vh and vw units for react native

--- packages/yoshiki/src/native/units.ts
+++ packages/yoshiki/src/native/units.ts
@@ -3,10 +3,23 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 //
 
-import { PixelRatio, Platform } from "react-native";
+import { Dimensions, PixelRatio, Platform } from "react-native";
 
 export const px = (value: number) => value;
 export const percent = (value: number) => `${value}%`;
-export const em = (value: number) =>
-	Platform.OS === "web" ? `${value}em` : PixelRatio.getFontScale() * 16 * value;
-export const rem = Platform.OS === "web" ? (value: number) => `${value}rem` : em;
+// prettier-ignore
+export const em = Platform.OS === "web"
+	? (value: number): number => `${value}em` as unknown as number
+	: (value: number): number => PixelRatio.getFontScale() * 16 * value;
+// prettier-ignore
+export const rem = Platform.OS === "web"
+	? (value: number) => `${value}rem` 
+	: em;
+// prettier-ignore
+export const vw = Platform.OS === "web"
+	? (value: number): number => `${value}vw` as unknown as number
+	: (value: number): number => value * Dimensions.get("window").width / 100;
+// prettier-ignore
+export const vh = Platform.OS === "web"
+	? (value: number): number => `${value}vh` as unknown as number
+	: (value: number): number => value * Dimensions.get("window").height / 100;
